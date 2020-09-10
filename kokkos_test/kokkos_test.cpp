7f1d993877dd33f55b829a740e357350bb81bea9
ctanis <craig-tanis@utc.edu>
2020-09-10 16:33:56 -0400

    working example

--- kokkos_test/kokkos_test.cpp
+++ kokkos_test/kokkos_test.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <Kokkos_Core.hpp>
 
-const int SIZE=1000000;
+const long int SIZE=1000000;
 
 int main(int argc, char *argv[])
 {
+
     Kokkos::initialize(argc, argv);
-
     {
+        std::cout << "This code will execute here: " <<
+            Kokkos::DefaultExecutionSpace::name() << std::endl;
 
-        //std::vector<int> data(SIZE, 0);
+        Kokkos::View<double*> data("my data", SIZE);
 
-        Kokkos::View<double> data("my data", SIZE);
+        Kokkos::parallel_for(SIZE, KOKKOS_LAMBDA(const int i) {
+                data(i)=i;
+            });
 
-        Kokkos::parallel_for(SIZE,
-                             KOKKOS_LAMBDA(const int i)
-                             {
-                                 data(i)=i;
-                             });
-    
+        double global_sum =0;
+        Kokkos::parallel_reduce(SIZE, KOKKOS_LAMBDA(const int i, double& sum) {
+                sum += data(i);
+            }, global_sum);
+            
+
+        std::cout << "analytic sum: " << SIZE*(SIZE-1)/2 << std::endl;
+        std::cout << "kokkos reduction: " << global_sum << std::endl;
+
     }
-    
     Kokkos::finalize();
     return 0;
 }
